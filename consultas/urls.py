8f69d19f806551613822061bb3a00c25d39d11a6
Sofia604 <sespana22@espol.edu.ec>
2020-05-24 17:11:03 -0400

    Configuracion de los estilos en los forms

--- consultas/urls.py
+++ consultas/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="consultas-home"),
     path('contactos/', views.contactos, name="consultas-contactos"),
-    path('informacion/', views.contactos, name="consultas-informacion"),
+    path('informacion/', views.informacion, name="consultas-informacion"),
     path('facebook_scrapper/', views.facebook, name="consultas-facebook"),
     path('instagram_scrapper/', views.instagram, name="consultas-instagram"),
 ]
